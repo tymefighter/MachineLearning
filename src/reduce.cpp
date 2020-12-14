2ac1c5e382c1e83f1eff20e1b5caa7ef61dfdcdc
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-12-14 17:17:51 +0530

    Add helper functions

--- src/reduce.cpp
+++ src/reduce.cpp
@@ -1,16 +1,22 @@
 #include <matrix.hpp>
 #include <reduce.hpp>
 
-void mulRow(Matrix &mat, double c) {
-    
+/** Multiplies ith row with constant c */
+void mulRow(Matrix &mat, int i, double c) {
+    for(int j = 0;j < mat.getNumCols();j++)
+        mat(i, j) *= c;
 }
 
+/** Adds jth row multiplied with constant c to the ith row */
 void addRow(Matrix &mat, int i, int j, double c) {
-
+    for(int k = 0;k < mat.getNumCols();k++)
+        mat(i, k) += mat(j, k) * c;
 }
 
+/** Swaps the ith and jth row */
 void swapRows(Matrix &mat, int i, int j) {
-
+    for(int k = 0;k < mat.getNumCols();k++)
+        swap(mat(i, k), mat(j, k));
 }
 
 Matrix getREF(Matrix &mat) {
