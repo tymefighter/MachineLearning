f32f360ed57ec6eb61a3109c08e4b947fb77e4cf
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-12-14 17:38:30 +0530

    Add REF using Gaussian Elimination

--- src/reduce.cpp
+++ src/reduce.cpp
@@ -20,5 +20,29 @@ void swapRows(Matrix &mat, int i, int j) {
 }
 
 Matrix getREF(const Matrix &mat) {
-   
+    int n = mat.getNumRows(), m = mat.getNumCols();
+    Matrix res(n, m);
+
+    int i = 0, j = 0;
+    while(i < n && j < m) {
+        if(res(i, j) == 0) {
+            int k = i + 1;
+
+            while(k < n && res(k, j) == 0)
+                k ++;
+
+            if(k < n) swapRows(res, i, k);
+            else {
+                j ++;
+                continue;
+            }
+        }
+
+        for(int k = i + 1;k < n;k++)
+            addRow(res, k, i, -res(k, j) / res(i, j));
+
+        i ++, j ++;
+    }
+
+    return res;
 }
