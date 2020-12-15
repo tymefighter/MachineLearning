87cc94b0abbbb55cb7fba4e695cbdd6d7877ea8c
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-12-15 13:37:24 +0530

    Add inverse computation code

--- src/linalg/operation.cpp
+++ src/linalg/operation.cpp
@@ -1,5 +1,8 @@
 #include <linalg/matrix.hpp>
+#include <linalg/reduce.hpp>
 #include <linalg/operation.hpp>
+
+/** Tranpose */
 
 Matrix transpose(const Matrix &mat) {
     int n = mat.getNumRows();
@@ -13,6 +16,8 @@ Matrix transpose(const Matrix &mat) {
 
     return resMat;
 }
+
+/** Matrix Multiplication */
 
 Matrix matmul(const Matrix &a, const Matrix &b) {
 
@@ -38,6 +43,8 @@ Matrix matmul(const Matrix &a, const Matrix &b) {
     return resMat;
 }
 
+/** Elementwise Multiplication */
+
 Matrix mul(const Matrix &a, const Matrix &b) {
 
     if(a.getShape() != b.getShape())
@@ -57,6 +64,8 @@ Matrix mul(const Matrix &a, const Matrix &b) {
     return resMat;
 }
 
+/** Elementwise Division */
+
 Matrix div(const Matrix &a, const Matrix &b) {
 
     if(a.getShape() != b.getShape())
@@ -74,4 +83,65 @@ Matrix div(const Matrix &a, const Matrix &b) {
     }
 
     return resMat;
+}
+
+/** Inverse */
+
+/**
+ * Computes Augmented Matrix consisting of the input
+ * matrix and the identity matrix. The input matrix
+ * is assumed to be a square matrix
+ * 
+ * @param mat The input matrix
+ * @return matrix [mat Identity]
+ */
+Matrix getAugmentedMatrix(const Matrix &mat) {
+    int n = mat.getNumRows();
+    Matrix augMat(n, 2 * n);
+
+    for(int i = 0;i < n;i++) {
+        for(int j = 0;j < n;j++)
+            augMat(i, j) = mat(i, j);
+
+        for(int j = n;j < 2 * n;j++)
+            augMat(i, j) = (i == j - n ? 1 : 0);
+    }
+
+    return augMat;
+}
+
+Matrix inverse(const Matrix &mat) {
+    if(mat.getNumRows() != mat.getNumCols())
+        throw MatError(
+            "Cannot Compute Inverse - "
+            "Number of Rows and Number of Cols do not match"
+        );
+
+    int n = mat.getNumRows();
+
+    // Get Augmented Matrix: [mat Identity]
+    Matrix augMat = getAugmentedMatrix(mat);
+
+    // Get RREF using Gauss-Jordan method
+    // rref = [R E]
+    Matrix rref = getRREF(augMat);
+
+    // If R in rref = [R E] is not identity, then
+    // inverse does not exist
+    for(int i = 0;i < n;i++) {
+        for(int j = 0;j < n;j++) {
+            if(rref(i, j) != (i == j ? 1 : 0))
+                throw MatError("Input Matrix is not invertible");
+        }
+    }
+
+    // Inverse exists and is E in rref = [R E]
+    // build the inverse and return it
+    Matrix matInv(n, n);
+    for(int i = 0;i < n;i++) {
+        for(int j = 0;j < n;j++)
+            matInv(i, j) = rref(i, j + n);
+    }
+
+    return matInv;
 }
--- src/linalg/operation.hpp
+++ src/linalg/operation.hpp
@@ -3,14 +3,50 @@
 
 #include <linalg/matrix.hpp>
 
+/**
+ * Computes the Transpose of the input matrix
+ * 
+ * @param mat Input Matrix
+ * @return The tranpose of the input matrix
+ */
 Matrix transpose(const Matrix &mat);
 
+/**
+ * Multiplies the input matrices
+ * 
+ * @param a First input matrix
+ * @param b Second input matrix
+ * @return result of matrix multiplication ab
+ */
 Matrix matmul(const Matrix &a, const Matrix &b);
 
+/**
+ * Multiplies the input matrices elementwise
+ * 
+ * @param a First input matrix
+ * @param b Second input matrix
+ * @return result of elementwise multiplication a * b
+ */
 Matrix mul(const Matrix &a, const Matrix &b);
 
+/**
+ * Divides the input matrices elementwise
+ * 
+ * @param a First input matrix
+ * @param b Second input matrix
+ * @return result of elementwise division a / b
+ */
 Matrix div(const Matrix &a, const Matrix &b);
 
+/**
+ * Computes the inverse of the input matrix. The input
+ * has to be a square matrix. If the matrix is not square
+ * or not invertible then a MatError is thrown
+ * 
+ * @param mat The input square matrix
+ * @return inverse of the input matrix if it exists, else
+ * an error is thrown
+ */
 Matrix inverse(const Matrix &mat);
 
 #endif
